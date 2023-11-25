ef36eb79222a7563a7531b52ca45d34756d72d29
Alberto Burgos Plaza <aburgos@cimne.upc.edu>
2023-11-25 15:36:12 +0100

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -7,30 +7,12 @@ import os
 BASE_DIR = os.environ.get("BASE_DIR", None)
 BASE_DIR = "." if BASE_DIR is None else BASE_DIR
 
-# Load packages from requirements.txt
-with open(f"{BASE_DIR}/requirements.txt") as file:
-    required_packages = [ln.strip() for ln in file.readlines()]
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-dev_packages = [
-    "black==21.9b0",
-    "flake8==3.9.2",
-    "ipykernel==5.5.5",
-    "ipython==7.16.1",
-    "isort==5.9.3",
-    "jedi==0.18.0",
-    "jupyter-client==7.0.2",
-    "jupyter-core==4.7.1",
-    "mypy-extensions==0.4.3",
-    "pyflakes==2.3.1",
-    "pylint==2.10.2",
-]
-
 setup(
     name="simplemailsender",
-    version="0.2.0",
+    version="0.3.0",
     license="Apache",
     description="A simple mail sender.",
     long_description=long_description,
@@ -41,10 +23,6 @@ setup(
     keywords=[
         "e-mail",
     ],
-    # python_requires=">=3.6",
     packages=find_namespace_packages(),
-    install_requires=[required_packages],
-    extras_require={
-        "dev": dev_packages,
-    },
+    python_requires=">=3.8",
 )
